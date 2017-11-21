bcb919fe5686ea61fa2d3fb30de44411a95bacb4
Christian Berendt <berendt@betacloud-solutions.de>
2017-11-21 15:50:11 +0100

    Add travis

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # docker-kolla-ansible
 
+[![Build Status](https://travis-ci.org/osism/docker-kolla-ansible.svg?branch=master)](https://travis-ci.org/osism/docker-kolla-ansible)
 [![Docker Hub](https://img.shields.io/badge/Docker%20Hub-osism%2Fkolla--ansible-blue.svg)](https://hub.docker.com/r/osism/kolla-ansible/)
 
 License
--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,25 +1,6 @@
 #!/usr/bin/env bash
 set -x
 
-PROJECT=kolla-ansible
-
-# Available environment variables
-#
-# OPENSTACK_VERSION
-# OSISM_VERSION
-# REPOSITORY
-
-# Set default values
-
-OPENSTACK_VERSION=${OPENSTACK_VERSION:-ocata}
-OSISM_VERSION=${OSISM_VERSION:-latest}
-REPOSITORY=${REPOSITORY:-osism/$PROJECT}
-
-TAG=$REPOSITORY:$OPENSTACK_VERSION-$OSISM_VERSION
-
-docker rmi "$TAG"
-docker rmi "$TAG-$(git rev-parse --short HEAD)"
-
 docker system prune -a -f
 
 # NOTE(berendt): not using git -d here to avoid removal of .vangrant and roles
