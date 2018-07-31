6b4fe086752a2e001ba18f0e9330b53b8a38ef13
Christian Berendt <berendt@betacloud-solutions.de>
2018-07-31 13:03:35 +0200

    Remove clean.sh script

--- scripts/clean.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -x
-
-docker system prune -a -f
-
-# NOTE(berendt): not using git -d here to avoid removal of .vangrant and roles
-rm -rf .venv release
-git clean -f -x
-git checkout .
